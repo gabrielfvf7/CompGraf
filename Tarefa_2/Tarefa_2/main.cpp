ad84a2591b9771876e553a84ad612562e8a62a0b
Gabriel <gabrielfvf@outlook.com>
2018-08-23 00:04:30 -0300

    Pontos não somem ao clicar

--- Tarefa_2/Tarefa_2/main.cpp
+++ Tarefa_2/Tarefa_2/main.cpp
@@ -12,15 +12,16 @@ float x = 0.0; //definicao de x como float para uso posterior
 float y = 0.0; //definicao de y como float para uso posterior
 float verticeX[1000]; //array usado para desenhar, usando o mouse, as coordenadas X 
 float verticeY[1000]; //array usado para desenhar, usando o mouse, as coordenadas Y 
-float posicaoPx = 0;  //posicao em X do ponto clicado
-float posicaoPy = 0;  //posicao em Y do ponto clicado
+float posicaoPx[1000];  //posicao em X do ponto clicado
+float posicaoPy[1000];  //posicao em Y do ponto clicado
 int n = 0; //quantidade de pontos criados com o mouse
+int p = 0;
 int i = 0; //variavel usada nos for do programa
 int v2 = 0; //ultimo vertice do poligono
 bool fechou = false; //variavel booleana usada para saber se o poligono foi fechado
 bool clicouPonto = false; //variavel booleana usada pra saber se foi clicado um ponto
 int intercepta = 0; //variavel usada para saber quantas vezes a resta ligando o ponto clicado a um fora da regiao intercepta o poligono
-float red, green = 0.0;
+float red[1000]; float green[1000];
 
 float prodVet(float Px, float Py, float Vx, float Vy) { //funcao usada para calcular o produto vetorial
 	float prodVet = Px * Vy - Py * Vx; //local onde calculo o produto vetorial
@@ -41,8 +42,8 @@ void fechar() { //funcao usada para fechar o poligno, usei a letra 'r' do teclad
 
 void dentro() { //funcao para descobrir se o ponto clicado pelo mouse esta dentro do poligono, ela eh chamada ao apertar a tecla 'p' do teclado
 	intercepta = 0; //como a funcao acabou de ser chamada, a quantidade de vezes que a aresta ligando o ponto a um ponto fora da regiao, intercepta o poligono, eh zero
-	float PQx = 1 - posicaoPx; //defino coordenada X da aresta que liga o ponto P e o ponto Q (fora da regiao do poligono)
-	float PQy = 1 - posicaoPy; //defino coordenada Y da aresta que liga o ponto P e o ponto Q (fora da regiao do poligono)
+	float PQx = 1 - posicaoPx[p]; //defino coordenada X da aresta que liga o ponto P e o ponto Q (fora da regiao do poligono)
+	float PQy = 1 - posicaoPy[p]; //defino coordenada Y da aresta que liga o ponto P e o ponto Q (fora da regiao do poligono)
 	float arestaX; //coordenada X da aresta do poligono a qual estarei verificando se foi interceptada por PQ
 	float arestaY; //coordenada Y da aresta do poligono a qual estarei verificando se foi interceptada por PQ
 	for (i = 0; i < n-1; i++) { //rodo um for para cada aresta do poligono
@@ -52,9 +53,9 @@ void dentro() { //funcao para descobrir se o ponto clicado pelo mouse esta dentr
 		//considerando como AB a aresta do ponto clicado com o ponto fora da regiao
 		//e CD como a aresta atual do poligono
 		//os produtos vetoriais a seguir serao:
-		float prodVet1 = prodVet(PQx, PQy, (verticeX[i] - posicaoPx), (verticeY[i] - posicaoPy)); //(AB x AD)
-		float prodVet2 = prodVet(PQx, PQy, (verticeX[i + 1] - posicaoPx), (verticeY[i + 1] - posicaoPy));//(AB x AC)
-		float prodVet3 = prodVet(arestaX, arestaY, (posicaoPx - verticeX[i]), (posicaoPy - verticeY[i]));//(CD x CA)
+		float prodVet1 = prodVet(PQx, PQy, (verticeX[i] - posicaoPx[p]), (verticeY[i] - posicaoPy[p])); //(AB x AD)
+		float prodVet2 = prodVet(PQx, PQy, (verticeX[i + 1] - posicaoPx[p]), (verticeY[i + 1] - posicaoPy[p]));//(AB x AC)
+		float prodVet3 = prodVet(arestaX, arestaY, (posicaoPx[p] - verticeX[i]), (posicaoPy[p] - verticeY[i]));//(CD x CA)
 		float prodVet4 = prodVet(arestaX, arestaY, (1 - verticeX[i]), (1 - verticeY[i]));//(CD x CB)
 
 		if ((prodVet1 * prodVet2) < 0 && (prodVet3 * prodVet4) < 0) { //aqui verifico se as duas arestas estao se interceptando
@@ -64,14 +65,14 @@ void dentro() { //funcao para descobrir se o ponto clicado pelo mouse esta dentr
 
 	if (intercepta % 2 == 0) { //se o numero de vezes que intercepta for par, o ponto esta fora do poligono
 		cout << "PONTO ESTA FORA DO POLIGONO! \n";
-		red = 1.0;
-		green = 0.0;
+		red[p] = 1.0;
+		green[p] = 0.0;
 		glutPostRedisplay();
 	}
 	else { //se o numero de vezes que intercepta for impar, o ponto esta dentro do poligono
 		cout << "PONTO ESTA DENTRO DO POLIGONO! \n";
-		red = 0.0;
-		green = 1.0;
+		red[p] = 0.0;
+		green[p] = 1.0;
 		glutPostRedisplay();
 	}
 }
@@ -89,10 +90,11 @@ void mouse(int button, int state, int X, int Y) { //funcao para pegar os cliques
 		else if (fechou == true) {
 			x = (2.*X) / 400 - 1.; //ajusto a coordenada de X
 			y = -(2.*Y) / 400 + 1.; //ajusto a coordenada de Y
-			posicaoPx = x; //coordenada X do ponto clicado apos o poligono fechar
-			posicaoPy = y; //coordenada Y do ponto clicado apos o poligono fechar
+			posicaoPx[p] = x; //coordenada X do ponto clicado apos o poligono fechar
+			posicaoPy[p] = y; //coordenada Y do ponto clicado apos o poligono fechar
 			clicouPonto = true; //atribuo o valor de true a essa variavel apos eu ter clicado um ponto dentro/fora do poligono
 			dentro();
+			p++;
 			glutPostRedisplay(); //reseto o display
 		}
 	}
@@ -118,14 +120,17 @@ void display() {
 	glColor3f(0.0, 0.0, 1.0);
 	glBegin(GL_LINE_STRIP); 
 		for (i = 0; i < n; i++) { 
-			glVertex2f(verticeX[i], verticeY[i]); //aqui desenho as arestas do poligono
+			glVertex2f(verticeX[i], verticeY[i]); //aqui desenho as arestas do poligono			
 		}
 		v2 = i;
 	glEnd();
 	if (fechou == true && clicouPonto == true) { //se o poligono estiver fechado e ter sido clicado um ponto com o mouse, desenhara esse ponto
-		glColor3f(red, green, 0.0);
+		
 		glBegin(GL_POINTS);
-		glVertex2f(posicaoPx, posicaoPy);
+		for (i = 0; i < p; i++) {
+			glColor3f(red[i], green[i], 0.0);
+			glVertex2f(posicaoPx[i], posicaoPy[i]);
+		}
 		glEnd();
 	}
 	glFlush();
